6face03721f5617f5dcbfe34da530cb9fc9e9c67
Gremious Grenald <gremious@gremy.co.uk>
2024-10-26 12:16:10 +0100

    Input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Deck File
+    #[arg(short)]
+    input: std::path::PathBuf,
 }
+
+fn main() {
+    let args = Args::parse();
+
+    println!("Hello, world!: {args:#?}");
+}
+
+// https://api.scryfall.com/cards/named?&format=image&image=png&exact=Adaptive Automaton&set=brr
+// async fn get_card_image()
